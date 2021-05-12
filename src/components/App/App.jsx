eff39cbd0f93b705151d2f701e5bdfca4ed8b025
xtianmarucco <christian.marucco@gmail.com>
2021-05-12 13:54:04 -0300

    feat(routing configuration): routing configuration complete
    routing configuration complete

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,1 +1,13 @@
-export const App = () => <>Hello from PortEXE</>;
+import { Switch, Route } from 'react-router-dom';
+
+import { Login, Signup, Chat } from 'components';
+
+export const App = () => {
+  return (
+    <Switch>
+      <Route path="/login" component={Login} />
+      <Route path="/signup" component={Signup} />
+      <Chat exact path="/" component={Chat} />
+    </Switch>
+  );
+};
